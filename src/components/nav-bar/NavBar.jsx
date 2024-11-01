22ba3d4d8bbf8c5c8b33fcf92a1760f504739a6c
Santiago Duque <santiago.duque.chacon@correounivalle.edu.co>
2024-11-01 17:18:30 -0500

    Hotfix NavBar.css import

--- src/components/nav-bar/NavBar.jsx
+++ src/components/nav-bar/NavBar.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import { Link } from "react-router-dom";
 import EXpatialMainLogoOnly from "../../assets/svgs/EXpatialMainLogoOnly.svg";
 import github_mark from "../../assets/svgs/github_mark.svg";
-import "./Navbar.css";
+import "./NavBar.css";
 
 function Navbar() {
   return (
