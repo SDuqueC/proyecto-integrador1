b7b9dd22afb9360603401e77172531597ada67c6
SDuqueC <santiago.duque.chacon@correounivalle.edu.co>
2024-11-01 11:56:05 -0500

    Added routes correctly

--- src/main.jsx
+++ src/main.jsx
@@ -1,30 +1,33 @@
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import Login from "./pages/login/Login.jsx";
-import Home from "./pages/home/Home.jsx";
-import About from "./pages/about/About.jsx";
-import EnvironmentalIssues from "./pages/environmental/EnvironmentalIssues.jsx";
-import App from "./App.jsx";
-import "./index.css";
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import Login from './pages/login/Login.jsx';
+import Home from './pages/home/Home.jsx';
+import About from './pages/about/About.jsx';
+import EnvironmentalIssues from './pages/environmental/EnvironmentalIssues.jsx';
+import App from './App.jsx';
+import './index.css';
 
 const router = createBrowserRouter([
   {
-    path: "/",
+    path: '/',
     element: <Login />,
   },
   {
-    path: "/home",
-    element: <App />, // Cambiamos App como el layout principal
-    children: [
-      { path: "", element: <Home /> }, // La ruta raíz (home) anidada
-      { path: "about", element: <About /> }, // Ruta relativa para About
-      { path: "environmental-issues", element: <EnvironmentalIssues /> }, // Ruta relativa para Environmental Issues
-    ],
+    path: '/home',
+    element: <App />,
+  },
+  {
+    path: '/about',
+    element: <About />,
+  },
+  {
+    path: '/issues',
+    element: <EnvironmentalIssues />,
   },
 ]);
 
-createRoot(document.getElementById("root")).render(
+createRoot(document.getElementById('root')).render(
   <StrictMode>
     <RouterProvider router={router} />
   </StrictMode>
