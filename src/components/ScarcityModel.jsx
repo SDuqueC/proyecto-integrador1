541ce8da9cc906480fc2b5f63040bfc4a782dd82
Santiago Duque <santiago.duque.chacon@correounivalle.edu.co>
2024-11-16 22:02:18 -0500

    Fix river model and add shadows

--- src/components/ScarcityModel.jsx
+++ src/components/ScarcityModel.jsx
@@ -1,6 +1,6 @@
 import React, { useRef } from "react";
 import { Canvas, useFrame } from "@react-three/fiber";
-import { useGLTF, Environment, PerspectiveCamera } from "@react-three/drei";
+import { useGLTF, Environment, PerspectiveCamera, ContactShadows } from "@react-three/drei";
 
 const RotatingModel = () => {
   const modelRef = useRef();
@@ -12,17 +12,25 @@ const RotatingModel = () => {
 
   const { scene } = useGLTF("/models/water_scarcity_model.glb");
 
+  scene.traverse((child) => {
+    if (child.isMesh) {
+      child.castShadow = true;
+      child.receiveShadow = true;
+    }
+  });
+
   return <primitive ref={modelRef} object={scene} scale={0.5} />;
 };
 
 const ScarcityModel = () => {
   return (
-    <Canvas style={{ width: "100%", height: "400px" }}>
-      <PerspectiveCamera makeDefault position={[0.5, 3, 10]} rotation={[-0.6,0,0]} />
+    <Canvas style={{ width: "100%", height: "400px" }} shadows>
+      <PerspectiveCamera makeDefault position={[0.5, 3, 10]} rotation={[-0.6, 0, 0]} />
       <ambientLight intensity={0.5} />
-      <directionalLight position={[0, 5, 5]} intensity={1} />
+      <directionalLight position={[0, 5, 5]} intensity={1} castShadow shadow-mapSize-width={2048} shadow-mapSize-height={2048} />
       <RotatingModel />
       <Environment preset="sunset" />
+      <ContactShadows position={[0, -2.5, 0]} opacity={0.6} scale={8} blur={1.5} far={6.5} />
     </Canvas>
   );
 };
