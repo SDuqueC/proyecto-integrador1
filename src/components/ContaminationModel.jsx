561358f541df75c9032b53dbe907b5999d717a8c
Santiago Duque <santiago.duque.chacon@correounivalle.edu.co>
2024-11-16 22:51:47 -0500

    Add sky, stars and sparkles to models

--- src/components/ContaminationModel.jsx
+++ src/components/ContaminationModel.jsx
@@ -1,6 +1,6 @@
 import React, { useRef } from "react";
 import { Canvas, useFrame } from "@react-three/fiber";
-import { useGLTF, Environment, PerspectiveCamera, ContactShadows } from "@react-three/drei";
+import { useGLTF, Environment, PerspectiveCamera, ContactShadows, Stars, Sparkles } from "@react-three/drei";
 
 const RotatingModelOpposite = () => {
   const modelRef = useRef();
@@ -25,12 +25,14 @@ const RotatingModelOpposite = () => {
 const ContaminationModel = () => {
   return (
     <Canvas style={{ width: "100%", height: "400px" }} shadows>
-      <PerspectiveCamera makeDefault position={[0, 5, 8]} rotation={[-0.3, 0, 0]} />
+      <PerspectiveCamera makeDefault position={[0, 5, 10]} rotation={[-0.3, 0, 0]} />
       <ambientLight intensity={0.5} />
       <directionalLight position={[0, 5, 5]} intensity={1} castShadow  />
       <RotatingModelOpposite />
       <Environment preset="forest" />
       <ContactShadows position={[0, -1.5, 0]} opacity={0.5} scale={10} blur={2.5} far={15.5} />
+      <Sparkles count={100} scale={9} size={3} speed={0.6} noise={2} />
+      {/* <Stars radius={100} depth={50} count={5000} factor={4} saturation={0} fade /> */}
       {/* <mesh receiveShadow rotation={[-Math.PI / 2, 0, 0]} position={[0, -2, 0]}>
         <planeGeometry args={[100, 100]} />
         <shadowMaterial opacity={0.3} />
